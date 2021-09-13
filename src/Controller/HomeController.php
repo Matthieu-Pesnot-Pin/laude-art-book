6dc14f14ab833308c58c5858cda0bd31a28e4186
Matthieu Pesnot-Pin <matthieupin@gmail.com>
2021-09-13 18:57:08 +0200

    Commit of the day

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'listeImage' => [
+                ["img" => "images-1.jpg", "infos" => "Couverture la petite échassière"],
+                ["img" => "images-2.jpg", "infos" => "Diptyque oiseaux"],
+                ["img" => "images-3.jpg", "infos" => "Pitichat deviendra grand"],
+                ["img" => "images-4.jpg", "infos" => "Piti renard"],
+            ],
+        ]);
+    }
+}
